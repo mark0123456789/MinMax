e54f3eaece71b9e7bcc63d42865e3522696ad668
mark <piringerm@kkszki.hu>
2024-10-11 10:51:20 +0200

    feladat befejezve

--- Program.cs
+++ Program.cs
@@ -8,8 +8,34 @@ namespace MinMax
 {
     internal class Program
     {
+        public static void MinMax(int[] bekertszam, out int min, out int max)
+        {
+            min = bekertszam[0];
+            max = bekertszam[0];
+            for (int i = 0; i < bekertszam.Length; i++)
+            {
+                if (bekertszam[i] < min)
+                {
+                    min = bekertszam[i];
+                }
+                if (bekertszam[i] > max)
+                {
+                    max = bekertszam[i];
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
+            var bekertszam = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            int min;
+            int max;
+
+
+            MinMax(bekertszam, out min, out max);
+            Console.WriteLine("Minimum: {0}, Maximum:{1}", min, max);
+            Console.ReadKey();
 
 
         }
